023faffa3520a1cc5b47a8c5f9932b804b61b453
titan_exasaur <tinytachyon14341@gmail.com>
2026-07-31 20:44:10 +0530

    added input validation for Nand - added _check_bits function - included _check_bits in Nand function

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,20 +1,24 @@
 from typing import Literal
 
-def Nand(a:Literal[0,1], b:Literal[0,1]) -> Literal[0,1]:
+def Nand(a:Literal[0,1], b:Literal[0,1]) -> Literal[0,1]:  # the building block of this project
     '''
     implementing a basic Nand gate
 
     Args:
         a - 1 bit value
-        b - 1 bt value
+        b - 1 bit value
     
     Returns:
         output - 1 bit value of (ab)'
     '''
-    if a and b not in (0,1):
-        raise ValueError('Values must be 0 or 1')
     
+    _check_bits(a,b)
+
     operation = not(a and b)
     val = 1 if operation else 0
     
     return val
+
+def _check_bits(*bits: int) -> None:
+    if any(bit not in (0, 1) for bit in bits):
+        raise ValueError("Values must be 0 or 1")
