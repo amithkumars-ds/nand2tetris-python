1a3dd2ae510e06137393a381119e82a1145b3eea
titan_exasaur <tinytachyon14341@gmail.com>
2026-07-31 20:44:56 +0530

    elementary chips complete - implemented not, and, or, xor, mux, demux chips - tested using hardcoded values

--- booleanLogic/elementaryChips.py
+++ booleanLogic/elementaryChips.py
@@ -1,9 +1,9 @@
 from typing import Literal
-from utilities.utils import Nand # the building block of this project
+from utilities.utils import Nand, _check_bits
 
 def Not(a: Literal[0,1]) -> Literal[0,1]:
     '''
-    performs not operation on input
+    performs not operation on 1 bit
 
     Args:
         a - 1 bit value
@@ -11,9 +11,127 @@ def Not(a: Literal[0,1]) -> Literal[0,1]:
     Returns:
         output is negation of a i.e., a'
     '''
-    if a not in (0,1):
-        raise ValueError("Value must be 0 or 1")
 
-    val = Nand(a=a,b=a)
-    return val
+    _check_bits(a)
 
+    out = Nand(a=a,b=a)
+    return out
+
+def And(a:Literal[0,1], b:Literal[0,1]) -> Literal[0,1]:
+    '''
+    performs and operation on 2 bits
+
+    Args:
+        a - 1 bit value
+        b - 1 bit value
+    
+    Returns:
+        output is AND operation between a and b ie., a.b
+    '''
+
+    _check_bits(a,b)
+    
+    not_ab = Nand(a=a,b=b)
+    out = Nand(a=not_ab,b=not_ab)
+
+    return out
+
+def Or(a:Literal[0,1], b:Literal[0,1]) -> Literal[0,1]:
+    '''
+    performs or operation on 2 bits
+
+    Args:
+        a - 1 bit value
+        b - 1 bit value
+
+    Returns:
+        output is OR operation between a and b ie., a + b
+    '''
+
+    _check_bits(a,b)
+
+    not_a = Nand(a=a,b=a)
+    not_b = Nand(a=b,b=b)
+    out = Nand(a=not_a,b=not_b)
+
+    return out
+
+def Xor(a:Literal[0,1], b:Literal[0,1]) -> Literal[0,1]:
+    '''
+    performs Xor operation on 2 bits
+
+    Args:
+        a - 1 bit value
+        b - 1 bit value
+
+    Returns:
+        output is XOR operation between a and b ie., a'b + ab'
+    '''
+
+    _check_bits(a,b)
+
+    Nand_ab = Nand(a=a,b=b)
+    not_a_Nand_ab = Nand(a=a,b=Nand_ab)
+    not_b_Nand_ab = Nand(a=Nand_ab,b=b)
+
+    out = Nand(a=not_a_Nand_ab,b=not_b_Nand_ab)
+
+    return out
+
+def Mux(a:Literal[0,1], b:Literal[0,1], sel:Literal[0,1]) -> Literal[0,1]:
+    '''
+    2:1 Multiplexer - selects one of the inputs
+
+    if,
+        sel=0, out=a
+        sel=1, out=b
+
+    Logic is: as' + bs
+
+    Args:
+        a - 1 bit value
+        b - 1 bit value
+        sel - 1 bit selector value
+
+    Returns:
+        output is selected from a and b depending on sel signal
+    '''
+
+    _check_bits(a,b,sel)
+
+    not_sel = Nand(a=sel,b=sel)
+    nand_a_not_sel = Nand(a=a,b=not_sel)
+    nand_b_sel = Nand(a=b,b=sel)
+
+    out = Nand(a=nand_a_not_sel,b=nand_b_sel)
+
+    return out
+
+def DeMux(D: Literal[0,1], sel: Literal[0,1]) -> tuple[Literal[0,1], Literal[0,1]]:    
+    '''
+    1:2  DeMultiplexer - splits input into 2 
+    
+    if,
+        sel=0, a=D, b=0
+        sel=1, a=0, b=D
+
+    Logic is: a=D.s' , b=D.s
+
+    Args:
+        D - 1 bit input value
+        sel - 1 bit selector value
+
+    Returns:
+        output is split into a and b using sel signal
+    '''
+
+    _check_bits(D,sel)
+
+    not_sel = Nand(a=sel,b=sel)
+    nand_D_not_sel = Nand(a=D,b=not_sel)
+    a = Nand(a=nand_D_not_sel,b=nand_D_not_sel)
+
+    nand_D_sel = Nand(a=D,b=sel)
+    b = Nand(a=nand_D_sel,b=nand_D_sel)
+
+    return a, b
