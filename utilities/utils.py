d9fe90df81c07e36dd28f89a92fa4ff99a98f1d4
titan_exasaur <tinytachyon14341@gmail.com>
2026-07-31 20:00:23 +0530

    Nand implementation - built nand gate - which will be used to develop elementary gates and further

---
+++ utilities/utils.py
@@ -0,0 +1,18 @@
+from typing import Literal
+
+def Nand(a:Literal[0,1], b:Literal[0,1]) -> Literal[0,1]:
+    '''
+    implementing a basic Nand gate
+
+    Args:
+        a - 1 bit value
+        b - 1 bt value
+    
+    Returns:
+        output - 1 bit value of (ab)'
+    '''
+    if a and b not in (0,1):
+        raise ValueError('Values must be 0 or 1')
+    
+    val = a and b
+    return not(val)
